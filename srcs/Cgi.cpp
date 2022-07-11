31f8b8cdbbb082a340f8002cd9eed495d3d40788
trhee <quu.temp@gmail.com>
2022-07-11 17:11:54 +0900

    facing secret error

--- srcs/Cgi.cpp
+++ srcs/Cgi.cpp
@@ -18,8 +18,8 @@ void					Cgi::init(Location location, Request& request){
 	// FIXME: getBody().length() 가 안됐던 이유는 우변이 size_t이기 때문이었습니다.
 	// 둘 중 뭐가 나을지는 뭐가 나을지는 아직 잘 모르겠는데, content-length는 리퀘스트에 있을수도 있고 없을수도 있으니까(transfer-encoding=chunked)
 	// body length를 to_string하는 게 낫지 않을까 싶어요
-    m_env["CONTENT_LENGTH"] = request.getHeaderValue("Content-Length");
-    //m_env["CONTENT_LENGTH"] = std::to_string(request.getBody().length());
+    // m_env["CONTENT_LENGTH"] = request.getHeaderValue("Content-Length");
+    m_env["CONTENT_LENGTH"] = std::to_string(request.getBody().length());
 
     m_env["SCRIPT_FILENAME"] = location._root + request.getUri().getPath();
     m_env["SCRIPT_NAME"] = request.getUri().getPath(); //실행할 파일 분리 필요 
@@ -31,6 +31,7 @@ void					Cgi::init(Location location, Request& request){
     m_env["SERVER_SOFTWARE"] = "Webserv/1.0";
     m_env["SERVER_PORT"] = request.getUri().getPort();
     m_env["SERVER_NAME"] = request.getUri().getHost();
+	m_env["HTTP_X_SECRET_HEADER_FOR_TEST"] = request.getHeaderValue("X-Secret-Header-For-Test");
 }
 
 char					**Cgi::envToChar() {
@@ -56,14 +57,17 @@ std::string		Cgi::runCgi(std::string cgiPath) {
 	
 	env = envToChar();
 	FILE *tmp = tmpfile();
+	FILE *msg = tmpfile();
+	int storeMsg = fileno(msg);
 	int cgiInput = fileno(tmp);
+	std::cout << "Size recieved: " << m_requestMsg.size() << std::endl;
 	write(cgiInput, m_requestMsg.c_str(), m_requestMsg.size());
 	lseek(cgiInput, 0, 0);
-	if (pipe(fd) == -1)
-	{
-		deleteEnv(env);
-		return ("Status: 500\r\n\r\n");
-	}
+	// if (pipe(fd) == -1)
+	// {
+	// 	deleteEnv(env);
+	// 	return ("Status: 500\r\n\r\n");
+	// }
 	if ((pid= fork()) == -1)
 	{
 		deleteEnv(env);
@@ -71,9 +75,9 @@ std::string		Cgi::runCgi(std::string cgiPath) {
 	}
 	else if (pid == 0) // 자식 
 	{
-		close(fd[0]);
+		// close(fd[0]);
 		dup2(cgiInput, STDIN_FILENO);
-		dup2(fd[1], STDOUT_FILENO);
+		dup2(storeMsg, STDOUT_FILENO);
 		if(execve(cgiPath.c_str(), NULL, env) == -1)
 		{
 			deleteEnv(env);
@@ -84,15 +88,16 @@ std::string		Cgi::runCgi(std::string cgiPath) {
 	{
 		wait(NULL);
 		char	buffer[32768] = {0}; // 32kb
-		close(fd[1]);
-		while (read(fd[0], buffer, 32768) > 0){
-			read(fd[0], buffer, 32768);
+		// close(fd[1]);
+		lseek(storeMsg, 0, 0);
+		while (read(storeMsg, buffer, 32767) > 0){
 			retCgi += buffer;
 			memset(buffer, 0, 32768);
 		}
 	}
-	close(fd[0]);
+	// close(fd[0]);
 	fclose(tmp);
+	fclose(msg);
 	deleteEnv(env);
 	return (retCgi);
 }
