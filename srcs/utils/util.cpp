96dfa677f7b5c23f1651c9b708514346a5833670
hyeonjin-dot <hyejung@c1r4s1.42seoul.kr>
2022-07-12 16:22:37 +0900

    to_string & stoi 추가

--- srcs/utils/util.cpp
+++ srcs/utils/util.cpp
@@ -55,3 +55,16 @@ void prn_prepend(const std::string& str, const std::string& ch) {
 unsigned int millisec(struct timeval& from, struct timeval& to) {
     return ((to.tv_sec - from.tv_sec) * 1000 + (to.tv_usec - from.tv_usec) / 1000);
 }
+
+template<typename T>
+std::string to_string(const T & value) {
+    std::ostringstream oss;
+    oss << value;
+    return oss.str();
+}
+
+int stoi(std::string & s) {
+    int i;
+    std::istringstream(s) >> i;
+    return i;
+}
--- srcs/utils/util.hpp
+++ srcs/utils/util.hpp
@@ -3,11 +3,15 @@
 
 #include <string>
 #include <vector>
+#include <sstream>
 
 std::string checkSemicolon(std::string input);
 void ft_split(std::string buf, std::vector<std::string> &info);
 std::string& trimSpace(std::string& str);
 void prn_prepend(const std::string& str, const std::string& ch);
 unsigned int millisec(struct timeval& from, struct timeval& to);
+template<typename T>
+std::string to_string(const T & value);
+int stoi(std::string & s);
 
 #endif
